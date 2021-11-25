eb28a8b3a21457dd0ac0011e29d353d809265915
Sergey <sergeygetman2020@gmail.com>
2021-11-25 12:05:11 +0200

    add paginasie pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/Post.Service";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 
 function App() {
@@ -22,11 +23,22 @@ function App() {
   const [filter, setFilter] = useState({sort: "", query: ""})
   const [modal, setModal] = useState(false)
   const sortedAndSearcedPost = usePosts(posts, filter.sort, filter.query)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+
+  let pagesArray = getPagesArray(totalPages)
+  console.log(pagesArray)
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+   const totalCount = response.headers["x-total-count"]
+    setTotalPages(getPageCount(totalCount, limit))
 
   })
+
+  console.log(totalPages)
 
   useEffect(() => {
     fetchPosts().then((response) => {
@@ -62,9 +74,21 @@ function App() {
         <Postlist remove={removePost} posts={sortedAndSearcedPost}
                   title={"Can you try create post ?"}/>
       }
+      <div className="page__wrapper">
+        {pagesArray.map(p =>
+
+          <span
+            onClick={() => setPage(p)}
+            key={p}
+            className={page === p ? "page page__current" : "page"}
+
+          >{p}</span>
+        )}
+      </div>
 
 
       }
+
       <Counters/>
     </div>
   )
